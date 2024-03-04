72dd367e07ad2448d4b175530d5d1ddabfe30d5f
jlotten <jenna.otten@gmail.com>
2024-03-04 13:40:25 -0800

    Initial set-up for PYME in nextjs 14

---
+++ app/(front)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+    <h2 className="text-2xl py-2 text-green-900">Pequeñas y Medianas Empresas | Small and Medium Sized Businesses</h2>
+      <h2 className="text-2xl py-2">Latest Products</h2>
+    </>
+  );
+}
